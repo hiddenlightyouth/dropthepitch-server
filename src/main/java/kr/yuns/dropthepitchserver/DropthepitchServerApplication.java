6c405646fa48d4abfc6ea9939bb243d02c488d60
Yun-Hwan Jeon <yunsol267@gmail.com>
2026-09-01 01:47:52 +0900

    feat: add BaseEntity and configure Asia/Seoul timezone

--- src/main/java/kr/yuns/dropthepitchserver/DropthepitchServerApplication.java
+++ src/main/java/kr/yuns/dropthepitchserver/DropthepitchServerApplication.java
@@ -1,10 +1,19 @@
 package kr.yuns.dropthepitchserver;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DropthepitchServerApplication {
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DropthepitchServerApplication.class, args);
