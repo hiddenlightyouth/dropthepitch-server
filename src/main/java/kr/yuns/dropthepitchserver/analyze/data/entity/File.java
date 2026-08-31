297f9661f82ebf51abaa00a30ab6947cbe55b6ab
garden <113418319+Garden0728@users.noreply.github.com>
2026-09-01 05:51:24 +0900

    feat : file 엔티티 , 리포지토리 추가 및 InputType Enum 정의(#3)
    InputType - JPG,PNG,WEBP,MP4,PDF,MD

--- src/main/java/kr/yuns/dropthepitchserver/analyze/data/entity/File.java
+++ src/main/java/kr/yuns/dropthepitchserver/analyze/data/entity/File.java
@@ -1,4 +1,36 @@
 package kr.yuns.dropthepitchserver.analyze.data.entity;
 
-public class File {
+import jakarta.persistence.*;
+import kr.yuns.dropthepitchserver.analyze.data.enums.InputType;
+import kr.yuns.dropthepitchserver.common.jpa.BaseEntity;
+import kr.yuns.dropthepitchserver.project.data.entity.Project;
+import lombok.*;
+
+@Entity
+@Table(name = "file")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+public class File extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "project_id", nullable = false, unique = true)
+    private Project project;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private InputType type;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer size;
 }
--- src/main/java/kr/yuns/dropthepitchserver/analyze/data/repository/FileRepository.java
+++ src/main/java/kr/yuns/dropthepitchserver/analyze/data/repository/FileRepository.java
@@ -1,4 +1,10 @@
 package kr.yuns.dropthepitchserver.analyze.data.repository;
 
-public interface FileRepository {
+import kr.yuns.dropthepitchserver.analyze.data.entity.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FileRepository extends JpaRepository<File, Long> {
+    Optional<File> findByProjectId(Long projectId);
 }
