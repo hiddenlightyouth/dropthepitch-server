5853dbbc3734062608b03eb245e8a03b3fe33c7c
garden <113418319+Garden0728@users.noreply.github.com>
2026-09-02 03:21:57 +0900

    feat : 프로젝트 조회시 필요한 리포지토리 메서드 추가(#11)

--- src/main/java/kr/yuns/dropthepitchserver/project/data/repository/ProjectRepository.java
+++ src/main/java/kr/yuns/dropthepitchserver/project/data/repository/ProjectRepository.java
@@ -3,5 +3,8 @@ package kr.yuns.dropthepitchserver.project.data.repository;
 import kr.yuns.dropthepitchserver.project.data.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    Optional<Project> findByIdAndUserEmail(Long id, String email);
 }
--- src/main/java/kr/yuns/dropthepitchserver/report/data/repository/ReportRepository.java
+++ src/main/java/kr/yuns/dropthepitchserver/report/data/repository/ReportRepository.java
@@ -3,5 +3,8 @@ package kr.yuns.dropthepitchserver.report.data.repository;
 import kr.yuns.dropthepitchserver.report.data.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    Optional<Report> findByProjectId(Long projectId);
 }
