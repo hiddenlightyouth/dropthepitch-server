5e70e3c7ab2f00e83cb8ce0d4418fba27d237ee2
Yun-Hwan Jeon <yunsol267@gmail.com>
2026-09-02 12:58:20 +0900

    refactor: refactor domain DTO to record (#9)

--- src/main/java/kr/yuns/dropthepitchserver/persona/data/dto/response/PersonaResponseDto.java
+++ src/main/java/kr/yuns/dropthepitchserver/persona/data/dto/response/PersonaResponseDto.java
@@ -1,65 +1,60 @@
 package kr.yuns.dropthepitchserver.persona.data.dto.response;
 
 import kr.yuns.dropthepitchserver.persona.data.enums.Gender;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class PersonaResponseDto {
-    private Long personaId;
-    private String name;
-    private Integer age;
-    private Gender gender;
-    private String imageUrl;
-    private List<String> tags;
+public record PersonaResponseDto(
+        Long personaId,
+        String name,
+        Integer age,
+        Gender gender,
+        String imageUrl,
+        List<String> tags,
 
-    private String nationality;
-    private String education;
-    private String academicTrack;
-    private String residence;
-    private String job;
-    private String militaryService;
-    private String family;
-    private String relationshipStatus;
+        String nationality,
+        String education,
+        String academicTrack,
+        String residence,
+        String job,
+        String militaryService,
+        String family,
+        String relationshipStatus,
 
-    private String income;
-    private String assets;
-    private String consumptionHabit;
-    private String coreValue;
+        String income,
+        String assets,
+        String consumptionHabit,
+        String coreValue,
 
-    private String personality;
-    private String mbti;
-    private String belief;
-    private String riskTolerance;
-    private String directionPreference;
-    private String sharingTendency;
+        String personality,
+        String mbti,
+        String belief,
+        String riskTolerance,
+        String directionPreference,
+        String sharingTendency,
 
-    private String hobby;
-    private String exercise;
-    private String dayNightPattern;
-    private String overseasTravel;
-    private String attentionSpan;
+        String hobby,
+        String exercise,
+        String dayNightPattern,
+        String overseasTravel,
+        String attentionSpan,
 
-    private String mobileOs;
-    private String aiLiteracy;
-    private String newTechAttitude;
-    private String infoSource;
+        String mobileOs,
+        String aiLiteracy,
+        String newTechAttitude,
+        String infoSource,
 
-    private String biggestPain;
-    private String churnTrigger;
-    private String signatureQuote;
-    private String communicationStyle;
-    private String transportation;
-    private String distrustPoint;
-    private String paymentTrigger;
-    private String influencer;
-    private String community;
-    private String healthStatus;
+        String biggestPain,
+        String churnTrigger,
+        String signatureQuote,
+        String communicationStyle,
+        String transportation,
+        String distrustPoint,
+        String paymentTrigger,
+        String influencer,
+        String community,
+        String healthStatus
+) {
 }
--- src/main/java/kr/yuns/dropthepitchserver/project/data/dto/response/SidebarProjectResponseDto.java
+++ src/main/java/kr/yuns/dropthepitchserver/project/data/dto/response/SidebarProjectResponseDto.java
@@ -1,17 +1,11 @@
 package kr.yuns.dropthepitchserver.project.data.dto.response;
 
 import kr.yuns.dropthepitchserver.project.data.enums.ProjectStatus;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class SidebarProjectResponseDto {
-    private Long projectId;
-    private String title;
-    private ProjectStatus status;
-}
+public record SidebarProjectResponseDto(
+        Long projectId,
+        String title,
+        ProjectStatus status
+) {}
