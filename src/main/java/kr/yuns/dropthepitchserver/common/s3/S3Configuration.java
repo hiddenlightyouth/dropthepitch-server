cef3c6c70d9ffcf7402795fa79d73af10c195ec1
백민수 <257681691+minsu33@users.noreply.github.com>
2026-09-03 03:35:22 +0900

    feat: S3 파일 업로드 서비스 구현 (#16)

---
+++ src/main/java/kr/yuns/dropthepitchserver/common/s3/S3Configuration.java
@@ -0,0 +1,42 @@
+package kr.yuns.dropthepitchserver.common.s3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+
+@Configuration
+public class S3Configuration {
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Value("${aws.credentials.access-key:}")
+    private String accessKey;
+
+    @Value("${aws.credentials.secret-key:}")
+    private String secretKey;
+
+    @Bean
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        if (StringUtils.hasText(accessKey) && StringUtils.hasText(secretKey)) {
+            return StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
+        }
+        return DefaultCredentialsProvider.create();
+    }
+
+    @Bean
+    public S3Client s3Client(AwsCredentialsProvider awsCredentialsProvider) {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(awsCredentialsProvider)
+                .build();
+    }
+}
